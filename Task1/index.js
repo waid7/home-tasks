85ce17aa0653cdfd764ca23862c506e8b200d059
dima <nevdaha505@gmail.com>
2018-11-04 13:43:30 +0200

    homework

--- Task1/index.js
+++ Task1/index.js
@@ -1,7 +1,7 @@
 var elems = document. getElementsByTagName('p');
 
 for (var i=0; i < elems.length; i++) {
-    elems[i].addEventListener('click', (function(temp) {
+    elems[i].addEventListener('click', (function() {
         return function(){
             console.log(this.innerText);
         }
