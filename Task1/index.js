672767ccac37d08ce4e4d5061bdbcc37883f0b40
dima <nevdaha505@gmail.com>
2018-11-05 17:12:06 +0200

    reworked script.js

--- Task1/index.js
+++ Task1/index.js
@@ -1,10 +1,10 @@
 var elems = document.getElementsByTagName('p');
 
 for (var i=0; i < elems.length; i++) {
-    elems[i].addEventListener('click', (function() {
+    elems[i].parentElement.addEventListener('click', (function() {
         return function(){
-            this.parentElement.style.background === "red" ? this.parentElement.style.background = "gray" : this.parentElement.style.background = "red";
-            console.log(this.innerText);
+            this.style.background === "red" ? this.style.background = "gray" : this.style.background = "red";
+            console.log(this.firstChild.innerText);
         }
     }()));
 };
