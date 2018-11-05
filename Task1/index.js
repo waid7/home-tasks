75107a34053c76a7b95f215e69434d2467687351
dima <nevdaha505@gmail.com>
2018-11-05 13:48:54 +0200

    added this functional

--- Task1/index.js
+++ Task1/index.js
@@ -3,7 +3,8 @@ var elems = document.getElementsByTagName('p');
 for (var i=0; i < elems.length; i++) {
     elems[i].addEventListener('click', (function() {
         return function(){
+            this.parentElement.style.background === "red" ? this.parentElement.style.background = "gray" : this.parentElement.style.background = "red";
             console.log(this.innerText);
         }
     }()));
-} 
+};
